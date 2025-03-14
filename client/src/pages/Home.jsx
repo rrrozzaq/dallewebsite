440e3b99b8f2c89314515b66475978da9585bfc7
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-03-14 14:44:19 +0700

    fix: add searchText

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,6 +15,8 @@ const RenderCards = ({ data, title}) => {
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState('');
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
