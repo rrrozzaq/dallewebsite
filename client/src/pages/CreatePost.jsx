d9cd5325818799bafe35973eedb4ae76e50e6aa0
Abdul Rozzaq <abdrozzaq02@gmail.com>
2025-03-15 23:09:01 +0700

    chore: setup create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setform] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setloading] = useState(false);
+
   return (
     <div>CreatePost</div>
   )
